dcee70a372addbc3d9c427656f83126fa703efe2
danielebifolco <79970043+danielebifolco@users.noreply.github.com>
2023-12-11 18:25:13 +0100

    newfile short

--- example.c
+++ example.c
@@ -1,21 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    // Declare a variable to store the number
     int number;
-
-    // Prompt the user to enter a number
     printf("Enter a number: ");
     scanf("%d", &number);
-
     // Check if the number is even or odd
-    if (number % 2) {
+    if (number % 2 == 0) {
         // If the remainder is 0, the number is even
         printf("%d is an even numer.\n", number);
-    } else {
-        // If the remainder is not 0, the number is odd
-        printf("%d is an odd number.\n", number);
     }
-
     return 0;
 }
