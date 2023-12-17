f226869fbfa4e95158a7cfa7d8e43096d980098b
danielebifolco <79970043+danielebifolco@users.noreply.github.com>
2023-12-17 18:51:36 +0100

    3nd commit on 4th example

--- ex_4/example.c
+++ ex_4/example.c
@@ -4,7 +4,7 @@ int main() {
     int x;
     char a[] = "Hello world!";
     scanf("%d", &x);
-    if (x>0 && a=NULL) {
+    if (x>1 && a=NULL) {
         printf("%s, %d è maggiore di 1.\n",a,x);
     }
     return 0;
